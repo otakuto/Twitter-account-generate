01acb1756ad2b062dc8bc0544e42b21e0fe2acb7
otakuto <otakuto5648@gmail.com>
2014-12-09 20:04:13 +0900

    a

---
+++ twitter.php
@@ -0,0 +1,55 @@
+<?php
+require 'TwistOAuth.php';
+//endpoint https://api.twitter.com/1/account/generate.json
+
+$i = 0;
+
+
+//Unique Username
+$username = "tteexxbalbala";
+//password
+$password = "wordpress";
+
+
+//”FØ—pTwitterID,PASS
+$id = "";
+$pass = "";
+
+
+//¶¬‚·‚éC‚Ì”
+while($i < 10){
+
+
+try {
+    
+
+    //OAuth”FØ
+    //Twitter for Android Sign-Up˜AŒg
+    $to = TwistOAuth::login('RwYLhxGZpMqsWZENFVw', 'Jk80YVGqc7Iz1IDEjCI6x3ExMSBnGjzBAH6qHcWJlo', $id, $pass);
+    //rotrors   dora0329
+
+    //API1???
+
+    $request = $to->post('https://api.twitter.com/1/account/generate.json',
+            array('screen_name' => $username.$i,
+                'password'=>$password,
+                'email'=>'ass'.rand().'bii'.rand().'@gmail.com',
+                'name'=>'we'.rand().'x'.rand(),
+                'protected'=>true //Œ®C
+            ));
+
+    //print_r($request);
+
+    print $request->screen_name." Created.".PHP_EOL;
+
+
+} catch (TwistException $e) {
+    print $e->getMessage().PHP_EOL;
+}
+
+    $i++;
+}
+
+
+
+?>
